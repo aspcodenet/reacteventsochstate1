44585131e1dcde5b9403cc9d969d6e44cb1c318b
stefan <stefan.holmberg@systementor.se>
2024-10-19 11:34:52 +0200

    first commit

---
+++ catshop/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+function App() {
+  const [activePage, setActivePage] = useState("Home")
+
+  const onItemClicked = (newPage)   =>{
+    setActivePage(newPage)
+}
+
+
+  return (
+    <>
+      <Header activePage={activePage} onItemClicked={onItemClicked}></Header>
+      <Main activePage={activePage}></Main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ catshop/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import { Home } from "./Home"
+import { Products } from "./Products"
+
+
+
+function Main(props) {
+  
+  return (
+    <>
+
+{  props.activePage == 'Home' && <Home></Home>}
+{  props.activePage == 'Products' && <Products></Products> }
+
+
+ </>
+  )
+}
+
+export default Main
