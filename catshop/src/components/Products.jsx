2e076cef183deea702d34369c1f053d3bca05d78
stefan <stefan.holmberg@systementor.se>
2024-10-23 14:51:53 +0200

    axios

--- catshop/src/components/Products.jsx
+++ catshop/src/components/Products.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
 
-import { products } from './data/ProductData' 
+import {useState,useEffect} from 'react'
+import { fetchProducts } from './data/ProductData' 
 import { Product } from './Product'
 
 // ta arrayen loopa den 
 // for varje skapa en <Product - komponent
 
 export const Products = () => {
+  const [products, setProducts] = useState([])
+
+
+   // BORDE VARA SÅ !!!!
+  // useEffect(() =>{
+  //     const result = await fetchProducts()
+  //     setProducts(result)
+  // }, []) // [] = en gång när asidan laddas
+
+
+  // SÅ HÄR KAN NI GÖRA
+  // const fetchData = async()=>{
+  //   const result = await fetchProducts()
+  //   setProducts(result)
+  // }
+  // useEffect(() =>{
+  //   fetchData()
+  // }, []) // [] = en gång när asidan laddas
+
+
+
+  // SÅ HÄR KAN BRUKAR MAN GÖRA
+  useEffect(() =>{
+    const fetchData = async()=>{
+      const result = await fetchProducts()
+      setProducts(result)
+    }
+    fetchData()
+  }, []) // [] = en gång när asidan laddas
+
+
   return (
 <section className="products" id="products">
             <h1 className="heading"> <span>our</span> products </h1>
