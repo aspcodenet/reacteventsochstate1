027431140906a5b523a1a539ec9807056cb5cd8a
stefan <stefan.holmberg@systementor.se>
2024-10-23 07:56:39 +0200

    yep

--- catshop/src/components/Products.jsx
+++ catshop/src/components/Products.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { Product } from './Product'
+
+import {useState,useEffect} from 'react'
+import {fetchProducts} from './Data/ProductData'
 
 export const Products = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() =>{
+    const fetchData = async()=>{
+      const result = await fetchProducts()
+      setProducts(result)
+    }
+    fetchData()
+  }, [])  
+
   return (
-    <div>Products</div>
+    <section className="products" id="products">
+            <h1 className="heading"> <span>our</span> products </h1>
+        
+            <div className="productgallery">
+                {
+                    products.map((theProduct,i)=> <Product key={i} product={theProduct} />    )
+                }
+            </div>
+        
+        </section>
   )
 }
